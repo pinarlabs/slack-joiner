527e5ab1332887fd17bd06a5056cafed0f6aef4f
pinarlabs <pinarlabs@users.noreply.github.com>
2020-10-06 23:33:35 -0300

    update styles and messages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,9 @@ export default function SlackForm() {
   const [email, setEmail] = useState("");
   const [end, setEnd] = useState(false);
   const [result, setResult]: [any, any] = useState({});
-
+  const divStyle = {
+    color: '#e25555' 
+  };
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -46,23 +48,27 @@ export default function SlackForm() {
         <Head>
           <script src="https://www.google.com/recaptcha/api.js" async defer></script>
           <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Ubuntu:regular,bold&subset=Latin"></link>
+          <title>{txt.welcomeMessage}</title>
         </Head>
         <form onSubmit={handleSubmit}>
-        <p>Bienvenidos al Slack de testingAR</p>
-          <div id="g-recaptcha-container">
-            <div className="g-recaptcha" data-sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_KEY}></div>
-          </div>
+          <div className="form-container">
+          <p>{txt.welcomeMessage}</p>
+            <div id="g-recaptcha-container">
+              <div className="g-recaptcha" data-sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_KEY}></div>
+            </div>
           
-          <div>
-            <input
-              type="email"
-              id="email"
-              placeholder={txt.form.emailMessage}
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <input id="invite" type="submit" value={txt.form.submitMessage} disabled={ email === "" }/>
+            <div>
+              <input
+                type="email"
+                id="email"
+                placeholder={txt.form.emailMessage}
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <input id="invite" type="submit" value={txt.form.submitMessage} disabled={ email === "" }/>
+            </div>
           </div>
+          <div className="madewith">made with <span style={divStyle}>&#9829;</span> by <a target="_blank" href="https://github.com/pinarlabs/slack-joiner">pinarlabs</a></div>
         </form>
       </>
 
